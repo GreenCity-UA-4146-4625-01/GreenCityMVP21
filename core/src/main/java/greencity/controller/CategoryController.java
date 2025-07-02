6c8a79d9f9071b4737e250f6bb8440336e4058c2
Sirobaba Mykyta <147396002+MykytaSirobaba@users.noreply.github.com>
2025-07-02 16:20:54 +0300

    Revert "fix(doc): add missing 400 Bad Request response for POST /econews"

--- core/src/main/java/greencity/controller/CategoryController.java
+++ core/src/main/java/greencity/controller/CategoryController.java
@@ -30,10 +30,10 @@ public class CategoryController {
      */
     @Operation(summary = "Save category")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = HttpStatuses.OK),
-        @ApiResponse(responseCode = "303", description = HttpStatuses.SEE_OTHER),
-        @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED),
-        @ApiResponse(responseCode = "400", description = HttpStatuses.BAD_REQUEST)
+            @ApiResponse(responseCode = "200", description = HttpStatuses.OK),
+            @ApiResponse(responseCode = "303", description = HttpStatuses.SEE_OTHER),
+            @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED),
+            @ApiResponse(responseCode = "400", description = HttpStatuses.BAD_REQUEST)
     })
     @PostMapping
     public ResponseEntity<CategoryDtoResponse> saveCategory(@Valid @RequestBody CategoryDto dto) {

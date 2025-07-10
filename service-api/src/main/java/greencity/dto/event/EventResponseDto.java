eed72e6e873d54368ae427469b54b404447c75dc
Linney34 <tarfedof@gmail.com>
2025-07-10 12:21:30 +0300

    Added more fields for EventResponseDto.

--- service-api/src/main/java/greencity/dto/event/EventResponseDto.java
+++ service-api/src/main/java/greencity/dto/event/EventResponseDto.java
@@ -1,11 +1,18 @@
 package greencity.dto.event;
 
+import greencity.enums.EventType;
+import greencity.enums.EventVisibility;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.hibernate.validator.constraints.URL;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -20,9 +27,31 @@ public class EventResponseDto {
     private Long eventId;
 
     @NotBlank
+    @Size(max = 70)
     private String title;
 
-    @NotEmpty
-    private LocalDateTime createdAt;
+    @NotBlank
+    @Size(min = 20, max = 63206)
+    private String description;
 
+    @NotNull
+    private EventVisibility visibility;
+
+    @NotEmpty
+    private Set<EventType> eventTypes;
+
+    @Size(min = 1, max = 7)
+    private List<@Valid EventDateTimeDto> eventDateTimes;
+
+    private List<@Valid EventLocationDto> locations;
+
+    private List<@URL String> onlineLinks;
+
+    @Size(max = 5)
+    private List<@Valid EventImageDto> images;
+
+    private Long mainImageId;
+
+    @NotNull
+    private LocalDateTime createdAt;
 }

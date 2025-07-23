990d59e6fd4da57ced44d221fcb5de90d67cd274
Linney34 <tarfedof@gmail.com>
2025-07-24 00:28:24 +0300

    Deleted unused impprt

--- dao/src/main/java/greencity/repository/options/EventCommentRepository.java
+++ dao/src/main/java/greencity/repository/options/EventCommentRepository.java
@@ -3,7 +3,6 @@ package greencity.repository.options;
 import greencity.entity.EventComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import org.springframework.data.domain.Page;

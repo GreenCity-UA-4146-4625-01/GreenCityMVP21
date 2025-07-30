9a3257261bed2a96db2816d0a0f629ca022a4bc6
mykytasirobaba <mykyta.sirobaba@gmail.com>
2025-07-31 01:36:49 +0300

    feat: create controller layer and add new rule to SecurityConfig

--- core/src/main/java/greencity/config/SecurityConfig.java
+++ core/src/main/java/greencity/config/SecurityConfig.java
@@ -195,7 +195,10 @@ public class SecurityConfig {
                     "/habit/assign/{habitAssignId}",
                     "/habit/tags/search",
                     "/habit/search",
-                    "/habit/{habitId}/friends/profile-pictures")
+                    "/habit/{habitId}/friends/profile-pictures",
+                    "/friends",
+                    "/friends/{friendId}/status",
+                    "/friends/search")
                 .hasAnyRole(USER, ADMIN, MODERATOR, UBS_EMPLOYEE)
                 .requestMatchers(HttpMethod.POST,
                     "/category",
@@ -217,7 +220,8 @@ public class SecurityConfig {
                     USER_SHOPPING_LIST,
                     "/user/{userId}/habit",
                     "/habit/custom",
-                    "/custom/shopping-list-items/{userId}/{habitId}/custom-shopping-list-items")
+                    "/custom/shopping-list-items/{userId}/{habitId}/custom-shopping-list-items",
+                    "/friends/{friendId}")
                 .hasAnyRole(USER, ADMIN, MODERATOR, UBS_EMPLOYEE)
                 .requestMatchers(HttpMethod.PUT,
                     "/habit/statistic/{id}",
@@ -240,7 +244,8 @@ public class SecurityConfig {
                     USER_SHOPPING_LIST + "/{shoppingListItemId}/status/{status}",
                     USER_SHOPPING_LIST + "/{userShoppingListItemId}",
                     "/user/profilePicture",
-                    "/user/deleteProfilePicture")
+                    "/user/deleteProfilePicture",
+                    "/friends/{friendId}/acceptFriend")
                 .hasAnyRole(USER, ADMIN, MODERATOR, UBS_EMPLOYEE)
                 .requestMatchers(HttpMethod.DELETE,
                     ECONEWS_COMMENTS,
@@ -251,7 +256,8 @@ public class SecurityConfig {
                     "/favorite_place/{placeId}",
                     "/social-networks",
                     USER_CUSTOM_SHOPPING_LIST_ITEMS,
-                    USER_SHOPPING_LIST + "/user-shopping-list-items")
+                    USER_SHOPPING_LIST + "/user-shopping-list-items",
+                    "/friends/{friendId}")
                 .hasAnyRole(USER, ADMIN, MODERATOR, UBS_EMPLOYEE)
                 .requestMatchers(HttpMethod.GET,
                     "/newsSubscriber",

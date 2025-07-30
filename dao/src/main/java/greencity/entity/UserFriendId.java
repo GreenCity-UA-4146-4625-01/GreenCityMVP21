cea80f4c2f7c9d1acf05b080cab6c44c983bd369
mykytasirobaba <mykyta.sirobaba@gmail.com>
2025-07-31 01:32:14 +0300

    feat: create changelog, update User entity, and add UserFriend entity with support class UserFriendId

---
+++ dao/src/main/java/greencity/entity/UserFriendId.java
@@ -0,0 +1,25 @@
+package greencity.entity;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Embeddable
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserFriendId implements Serializable {
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "friend_id")
+    private User friend;
+}
+
